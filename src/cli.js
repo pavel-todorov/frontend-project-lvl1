f882a9d665ebd2a9d1ad808679a3cef3992a3b71
pavel.todorov <pavel.todorov@hiq.ru>
2021-01-09 09:34:51 +0300

    fix lint issues

--- src/cli.js
+++ src/cli.js
@@ -11,18 +11,18 @@ const requestCycle = async (questionsCount, rules, functionality) => {
 	console.log(rules);
 	let correctAnswers = 0;
 	/* eslint-disable no-await-in-loop */
-	while (correctAnswers < questionsCount) {
-		const question = functionality();
-		console.log(`Question: ${question.questionString}`);
-		const result = await promptly.prompt('Your answer: ');
-		if (result === question.correctAnswer) {
-			console.log('Correct!');
-			correctAnswers += 1;
-		} else {
-			console.log(`'${result}' is wrong answer ;(. Correct answer was '${question.correctAnswer}'.\nLet's try again, ${name}!`);
-			return;
-		}
-	}
+  while (correctAnswers < questionsCount) {
+    const question = functionality();
+    console.log(`Question: ${question.questionString}`);
+    const result = await promptly.prompt('Your answer: ');
+    if (result === question.correctAnswer) {
+      console.log('Correct!');
+      correctAnswers += 1;
+    } else {
+      console.log(`'${result}' is wrong answer ;(. Correct answer was '${question.correctAnswer}'.\nLet's try again, ${name}!`);
+      return;
+    }
+  }
 	console.log(`Congratulations, ${name}!`);
 };
 
