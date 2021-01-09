714ec066cea854be8f0cfac4e988136403bfa58c
pavel.todorov <pavel.todorov@hiq.ru>
2021-01-09 09:08:09 +0300

    fix brain gcd

--- src/mathUtils.js
+++ src/mathUtils.js
@@ -8,7 +8,7 @@ function getRandomIntFromDiapazone(min = 0, max = DEFAULT_MAX_RANDOM) {
 
 const getMaxDivisor = (arg1, arg2) => {
     let res = 1;
-    for (let i = 2; i < Math.min(arg1, arg2); i += 1) {
+    for (let i = 2; i <= Math.min(arg1, arg2); i += 1) {
         if ((arg1 % i === 0) && (arg2 % i === 0)) {
             res = i;
         }
